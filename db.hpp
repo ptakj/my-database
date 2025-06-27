b15ba605b130d3170adaa6fa9725a426f1c89a08
jptak04 <ptakjakub004@gmail.com>
2025-06-27 22:55:56 +0200

    Start

---
+++ db.hpp
@@ -0,0 +1,11 @@
+#include<vector>
+
+
+
+template <typename T>
+class record
+{
+    protected:
+        std::vector<int> types_of_fields;
+
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "db.hpp"
+
+
+int main()
+{
+
+
+
+    return 0;
+}
