8039ed725547fc99de8c6df215b7447fe8ceeca3
jptak04 <ptakjakub004@gmail.com>
2025-07-03 15:01:09 +0200

    Header files

--- Field.hpp
+++ Field.hpp
@@ -1,13 +1,15 @@
+#pragma once
+#include<iostream>
+
+
 template<typename T>
 class Field
 {
     protected:
         T value;
     public:
-        Field(T value_)
-        {
-            value = value_;
-        }
+        Field(T value_);
+
         T value_get();
         void value_set(T new_value);
 };
